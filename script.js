720caf1e9f07beae680da6b9a5ef1b54c2d1697d
Leandro Nascimento <92461726+Leandro19Costa@users.noreply.github.com>
2022-04-01 00:21:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function alterarTema() {
+  document.body.classList.toggle("dark");
+}
+
+// //Conteúdo detalhado desta aula
+// Aprendemos a mexer no Figma e transformar o design em código
+// Entendemos melhor como funciona HTML e CSS
+// Estruturamos o nosso portfólio com HTML, aprendendo todas as tags necessárias pra isso
+// Estilizamos o nosso portfólio com CSS, conhecendo os seletores, propriedades e valores necessários para isso
+
+// Desafios desta aula!
+// Terminar todo o design que está no figma (disponível abaixo)
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+
+// Links importantes para você acompanhar a aula
+
+// Design no Figma
+//https://www.figma.com/file/1flmz2iauuNs8JsY6eaBHI/Imers%C3%A3o-Dev---Aula-9?node-id=53%3A2
+
+// Conteúdos extras
+
+// Propriedades do Flexbox - guia
+//https://www.alura.com.br/artigos/css-guia-do-flexbox
+
+///  LIVES
+
+/// Como se preparar entrevista
+//https://www.youtube.com/watch?v=ugKLcu1LRx8
+
+/// Colocando o prejeto na VERCEL
+//https://www.youtube.com/watch?v=YhQt8yjetZU
+
+///Proximos passos carreira TI DEV
+//https://www.youtube.com/watch?v=vdShnh18KGM
